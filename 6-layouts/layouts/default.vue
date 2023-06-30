1acc4127917be73cd2ae0e1af11d0ada9c7d5a92
yenilikci <m.melihcelik017@outlook.com>
2023-06-30 14:09:07 +0300

    lesson 6

---
+++ 6-layouts/layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/products">Products</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+
+  <div>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
---
+++ 6-layouts/layouts/products.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/products">Nuxt Dojo Merch</NuxtLink>
+      </nav>
+    </header>
+  </div>
+
+  <div>
+    <slot />
+  </div>
+
+  <footer>
+    <ul>
+      <li>
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/products">Products</NuxtLink>
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
