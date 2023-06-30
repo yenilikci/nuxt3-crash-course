590e6a9886e31d4f9205b487f1cd20fb8cb008b6
yenilikci <m.melihcelik017@outlook.com>
2023-06-30 14:25:09 +0300

    lesson 7

---
+++ 7-adding-tailwind-css/pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+
+definePageMeta({
+  layout: "products",
+});
+</script>
+
+<style scoped></style>
---
+++ 7-adding-tailwind-css/pages/products/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>Products</p>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "products",
+});
+</script>
+
+<style scoped></style>
