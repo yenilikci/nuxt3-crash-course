7c8682a38571c05919d58907289aaf9ee646ae70
yenilikci <m.melihcelik017@outlook.com>
2023-06-30 16:44:43 +0300

    lesson 11

---
+++ 11-metadata-usehead/nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  app: {
+    head: {
+      title: 'Nuxt Dojo',
+      meta: [
+        { name: 'description', content: 'Everything about Nuxt 3' }
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+      ]
+    }
+  },
+  devtools: { enabled: false }
+})
